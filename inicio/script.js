d308fa25f0931aeb37ab7e0173e6ba58ef7b3d1f
Fernando Mercado <fermercado@live.com>
2022-05-21 16:44:55 -0300

    organizando area de trabalho

--- inicio/script.js
+++
@@ -1,4 +0,0 @@
-import quadrado from './quadrado.js';
-import numeroAleatorio from './numeroAleatoria.js';
-console.log(quadrado.perimetroQuadrado(5));
-console.log(numeroAleatorio());
---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return <a href="https://fermercado.github.io/F1-fans/">F1</a>;
+};
+
+export default App;

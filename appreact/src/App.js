eb55db26de5eef457ded86cf7fc385995a246ab6
Fernando Mercado <fermercado@live.com>
2022-05-25 10:26:59 -0300

    componentes

--- appreact/src/App.js
+++ appreact/src/App.js
@@ -1,5 +1,24 @@
+import React, { fragment } from 'react';
+import Footer from './Footer';
+import Form from './Form/Form';
+import Header from './header';
+const Teste = () => {
+  const active = false;
+  if (active) {
+    return <p>Teste</p>;
+  } else {
+    return null;
+  }
+};
 const App = () => {
-  return <section></section>;
+  return (
+    <React.Fragment>
+      <Teste />
+      <Header />
+      <Form />
+      <Footer />
+    </React.Fragment>
+  );
 };
 
 export default App;

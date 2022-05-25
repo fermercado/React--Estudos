2b2c05c57bec32d6a0fe2dce96edad30c6cb6fb1
Fernando Mercado <fermercado@live.com>
2022-05-25 15:23:19 -0300

    desafio componentes

--- meu-app/src/App.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const App = () => {
-  return <div>React app</div>;
-};
-
-export default App;
--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './App.css';
-
-const App = () => {
-  return <a href="https://fermercado.github.io/F1-fans/">F1</a>;
-};
-
-export default App;

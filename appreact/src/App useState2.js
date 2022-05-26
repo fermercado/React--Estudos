0768c6263e84fdd613ed74842555fde03dcb45fc
Fernando Mercado <fermercado@live.com>
2022-05-26 14:45:54 -0300

    useState2

---
+++ appreact/src/App useState2.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const App = () => {
+  const [contar, setContar] = React.useState(1);
+  const [items, setItems] = React.useState(['Item 1']);
+
+  function handleClick() {
+    setContar((contar) => {
+      return contar + 1;
+    });
+    setItems((items) => [...items, 'Item ' + (contar + 1)]);
+  }
+  return (
+    <div>
+      {items.map((item) => (
+        <li key={item}>{item}</li>
+      ))}
+      <button onClick={handleClick}>{contar}</button>
+    </div>
+  );
+};
+
+export default App;
--- appreact/src/App.js
+++ appreact/src/App.js
@@ -1,21 +1,7 @@
 import React from 'react';
-import ButtonModal from './ButtonModal';
-import Modal from './Modal';
 
 const App = () => {
-  const [modal, setModal] = React.useState(false);
-  const [items, setItems] = React.useState('teste');
-
-  function handleClick() {
-    setItems('Outro');
-  }
-  return (
-    <div>
-      <button onClick={handleClick}>Clicar</button>
-      <Modal modal={modal} setModal={setModal} />
-      <ButtonModal setModal={setModal} />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;

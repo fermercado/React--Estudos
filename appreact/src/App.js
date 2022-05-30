ad4c03e8bbe444d5ac5d2e0ab7bd823516e08557
Fernando Mercado <fermercado@live.com>
2022-05-30 15:12:51 -0300

    updade

--- appreact/src/App.js
+++ appreact/src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import ProdutoEffect from './ProdutoEffect';
 
 const App = () => {
-  return <div></div>;
+  const [ativo, setAtivo] = React.useState(false);
+
+  return (
+    <div>
+      {ativo && <ProdutoEffect />}
+      <button onClick={() => setAtivo(!ativo)}>Ativar</button>
+    </div>
+  );
 };
 
 export default App;
